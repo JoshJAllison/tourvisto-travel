1a60505e35a335b4c20186dc6fb473912eb5f692
JoshJAllison <Joshua.Allison25@outlook.com>
2025-08-22 00:47:52 +0100

    Integrate SidebarComponent into AdminLayout and enhance NavItems to dynamically render sidebar links from constants.

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,4 +1,5 @@
 import {Link} from "react-router";
+import { sidebarItems } from "~/constants";
 
 const NavItems = () => {
   return (
@@ -7,6 +8,15 @@ const NavItems = () => {
             <img src="/assets/icons/logo.svg" alt="logo" className="size-[30px]" />
             <h1>Tourvisto</h1>
         </Link>
+
+        <div className="container">
+            <nav>
+              {sidebarItems.map(({id, href, icon, label}) => (
+                <div>{label}</div>
+              ))}
+            </nav>
+        </div>
+
     </section>
   )
 }
