4cc751b02e987f6378fa65826b5f8e314659706e
JoshJAllison <Joshua.Allison25@outlook.com>
2025-08-25 00:11:29 +0100

    Refactor MobileSidebar to use 'let' for sidebar declaration and implement toggle functionality for improved user interaction.

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 import type {SidebarComponent} from "@syncfusion/ej2-react-navigations"
 
 const MobileSidebar = () => {
-    const sidebar: SidebarComponent
+    let sidebar: SidebarComponent
 
   return (
     <div className="mobile-sidebar wrapper">
@@ -17,7 +17,10 @@ const MobileSidebar = () => {
                 <h1>Tourvisto</h1>
             </Link>
 
-            <button onClick={() => }></button>
+            {/*// @ts-ignore*/}
+            <button onClick={() => sidebar.toggle()}>
+                <img src="/assets/icons/menu.svg" alt="menu" className="size-7"/>
+            </button>
         </header>
     </div>
   )
