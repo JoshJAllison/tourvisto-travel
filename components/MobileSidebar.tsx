83b5e3049e1053d77d66c6dda924148bb0b6ff9f
JoshJAllison <Joshua.Allison25@outlook.com>
2025-08-25 00:09:12 +0100

    Implement MobileSidebar header with logo and navigation link for improved mobile user experience.

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,6 +1,25 @@
+import { Link } from "react-router";
+import type {SidebarComponent} from "@syncfusion/ej2-react-navigations"
+
 const MobileSidebar = () => {
+    const sidebar: SidebarComponent
+
   return (
-    <div>MobileSidebar</div>
+    <div className="mobile-sidebar wrapper">
+        <header>
+            <Link to="/">
+                <img
+                    src="/assets/icons/logo.svg"
+                    alt="logo"
+                    className="size-[30px]"
+                />
+
+                <h1>Tourvisto</h1>
+            </Link>
+
+            <button onClick={() => }></button>
+        </header>
+    </div>
   )
 }
 
