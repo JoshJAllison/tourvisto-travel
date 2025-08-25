4561506b96b83e6e1f34d067986fbb8c152b6dff
JoshJAllison <Joshua.Allison25@outlook.com>
2025-08-25 20:45:10 +0100

    Update Header in AllUsers component to include title and description for improved context and user engagement.

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -3,7 +3,10 @@ import { Header } from "../../../components";
 const AllUsers = () => {
   return (
     <main className="dashboard wrapper">
-      <Header />
+      <Header 
+        title="Trips Page"
+        description="Check out our current users in real time"
+      />
       All Users Page Contents
     </main>
   );
