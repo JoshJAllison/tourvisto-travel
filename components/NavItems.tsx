c9c9c5ff5ae373435bd1100c6d2ad199b812d9c1
JoshJAllison <Joshua.Allison25@outlook.com>
2025-08-25 20:02:13 +0100

    Enhance NavItems component by adding a handleClick prop for improved interactivity and user experience.

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -2,7 +2,7 @@ import { Link, NavLink } from "react-router";
 import { sidebarItems } from "~/constants";
 import { cn } from "~/lib/utils";
 
-const NavItems = () => {
+const NavItems = ({ handleClick }: { handleClick?: () => void }) => {
   const user = {
     name: "James",
     email: "support@tourvisto.pro",
