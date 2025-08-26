ae8c177df4fa833e4a01196418d2afcad2a936d7
JoshJAllison <Joshua.Allison25@outlook.com>
2025-08-26 23:26:30 +0100

    Update Header component to adjust description styling based on the current route for improved readability and consistency.

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,7 +15,9 @@ const Header = ({title, description}: Props) => {
             <article>
                 <h1 className={cn("text-dark-100", location.pathname === '/' ? 'text-2xl md:text-4xl font-bold':
                      'text-xl md:text-2xl font-semibold')}>{title}</h1>
-                <p>{description}</p>
+
+                <p className={cn("text-grey-100 font-normal", location.pathname === '/' ? 'text-base md:text-lg':
+                     'text-sm md:text-lg')}>{description}</p>
             </article>
         </header>
     )
