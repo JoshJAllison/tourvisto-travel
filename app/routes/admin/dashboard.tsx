cace5dac65835f0ce546b1f892f32554f1c77f7b
JoshJAllison <Joshua.Allison25@outlook.com>
2025-08-29 23:57:31 +0100

    Add dummy static data for trips in Dashboard component to prepare for dynamic implementation in the live version.

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,6 +1,7 @@
 import { Header, StatsCard, TripCard } from "../../../components";
 
-const Dashboard = () => {
+
+// Dummy static data (dynamic in live version)
   const user = { name: "James" };
   const dashboardStats = {
     totalUsers: 12450,
@@ -8,7 +9,45 @@ const Dashboard = () => {
     totalTrips: 3210,
     tripsCreated: { currentMonth: 150, lastMonth: 250 },
     userRole: { total: 62, currentMonth: 25, lastMonth: 15 },
-  };
+  }
+
+  const allTrips = [{
+    id: 1,
+    name: "Tropical Rewind",
+    imageUrls: ["/assets/images/sample1.jpg"],
+    itinerary: [{ location: "Thailand" }],
+    tags: ["Adventure", "Culture"],
+    travelStyle: "Solo",
+    estimatedPrice: "$1,000",
+  },
+  {
+    id: 2,
+    name: "French Reverie",
+    imageUrls: ["/assets/images/sample2.jpg"],
+    itinerary: [{ location: "Paris" }],
+    tags: ["Relaxation", "Culinary"],
+    travelStyle: "Family",
+    estimatedPrice: "$2,000",
+  },
+  {
+    id: 3,
+    name: "Zen Break",
+    imageUrls: ["/assets/images/sample3.jpg"],
+    itinerary: [{ location: "Japan" }],
+    tags: ["Shopping", "Luxury"],
+    travelStyle: "Couple",
+    estimatedPrice: "$3,000",
+  },
+  {
+    id: 4,
+    name: "Adventure in Westeros",
+    imageUrls: ["/assets/images/sample4.jpg"],
+    itinerary: [{ location: "Croatia" }],
+    tags: ["Historical", "Culture"],
+    travelStyle: "Friends",
+    estimatedPrice: "$4,000",
+  },
+  ];
 
   const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } =
     dashboardStats;
