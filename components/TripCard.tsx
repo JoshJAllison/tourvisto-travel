66468b924c49eeeb2fc8c2bffe4d36ff5c042a5f
JoshJAllison <Joshua.Allison25@outlook.com>
2025-08-30 21:18:20 +0100

    Enhance TripCard component to accept props for dynamic trip data display, improving the rendering of trip information in the Dashboard.

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -1,6 +1,12 @@
-const TripCard = () => {
+import {Link, useLocation} from "react-router";
+
+const TripCard = ({id, name, location, imageUrl, tags, price}: TripCardProps) => {
+  const path = useLocation();
+
   return (
-    <div>TripCard</div>
+    <Link to={}>
+      <img src={imageUrl} alt={name} />
+    </Link>
   )
 }
 
