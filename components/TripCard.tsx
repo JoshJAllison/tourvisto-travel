959717620d784e682dfdca1e0b3e9474ec87ea45
JoshJAllison <Joshua.Allison25@outlook.com>
2025-08-31 21:50:58 +0100

    Refactor TripCard component to improve tag rendering with corrected function usage and updated ChipListComponent ID, enhancing the display of trip tags.

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -1,5 +1,6 @@
 import {Link, useLocation} from "react-router";
-import { cn } from "~/lib/utils";
+import { cn, getFirstWord } from "~/lib/utils";
+import { ChipListComponent, ChipsDirective, ChipDirective } from "@syncfusion/ej2-react-buttons";
 
 
 const TripCard = ({id, name, location, imageUrl, tags, price}: TripCardProps) => {
@@ -21,16 +22,16 @@ const TripCard = ({id, name, location, imageUrl, tags, price}: TripCardProps) =>
       </article>
 
       <div className="mt-5 pl-[18px] pr-3.5 pb-5">
-        <ChipListComponent id="travel">
+        <ChipListComponent id="travel-chip">
           <ChipsDirective>
             {tags.map((tag, index) => (
               <ChipDirective
                 key={index}
-                text={getFirstWord{tag}}
+                text={getFirstWord(tag)}
                 cssClass={cn(index === 1 ? '!bg-pink-50 !text-pink-500' : '!bg-success-50 !text-success-700')}
               />
             ))}
-          </CHipsDirective>
+          </ChipsDirective>
         </ChipListComponent>   
       </div>
 
