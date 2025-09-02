dd670613c4a3faeca0f4904d84bc7d70bd3af37a
JoshJAllison <Joshua.Allison25@outlook.com>
2025-09-02 23:45:30 +0100

    Add Appwrite client initialization to client.ts for improved API interaction.

--- app/appwrite/client.ts
+++ app/appwrite/client.ts
@@ -1,3 +1,5 @@
+import { Client } from "appwrite";
+
 export const appwriteConfig = {
     endpointUrl: import.meta.env.VITE_APPWRITE_API_ENDPOINT,
     projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
@@ -6,3 +8,5 @@ export const appwriteConfig = {
     userCollectionId: import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID,
     tripCollectionId: import.meta.env.VITE_APPWRITE_TRIPS_COLLECTION_ID,
 }
+
+const client = new Client();
