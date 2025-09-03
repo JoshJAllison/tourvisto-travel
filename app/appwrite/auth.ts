66d02c18ec5242cc92d7ba3ee5d96424c0374ed9
JoshJAllison <Joshua.Allison25@outlook.com>
2025-09-03 16:23:12 +0100

    Add authentication functions for user management in Appwrite client

--- app/appwrite/auth.ts
+++ app/appwrite/auth.ts
@@ -5,3 +5,43 @@ export const loginWithGoogle = () => {
         console.log(e);
     }
 }
+
+export const logoutUser = () => {
+    try{
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export const getUser = () => {
+    try{
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export const getGooglePicture = () => {
+    try{
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export const storeUserData = () => {
+    try{
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export const getExistingUser = () => {
+    try{
+
+    } catch (e) {
+        console.log(e);
+    }
+}
