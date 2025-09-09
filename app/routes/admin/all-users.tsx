c9d175e38bdb55c72674bbee29d3b2de7335d2f5
JoshJAllison <Joshua.Allison25@outlook.com>
2025-09-09 23:18:30 +0100

    Expand All Users page by adding additional column directives to GridComponent for email, date joined, trip created, and user status, enhancing data presentation and user management capabilities.

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,15 +1,19 @@
 import { users } from "~/constants";
 import { Header } from "../../../components";
-import { ColumnsDirective, ColumnDirective, GridComponent } from "@syncfusion/ej2-react-grids";
+import {
+  ColumnsDirective,
+  ColumnDirective,
+  GridComponent,
+  Column,
+} from "@syncfusion/ej2-react-grids";
 
 const AllUsers = () => {
   return (
     <main className="all-users wrapper">
-      <Header 
+      <Header
         title="Manage Users"
         description="Filter, sort and access detailed user profiles"
       />
-
 
       <GridComponent dataSource={users} gridLines="None">
         <ColumnsDirective>
@@ -20,9 +24,48 @@ const AllUsers = () => {
             textAlign="Left"
             template={(props: UserData) => (
               <div className="flex items-center gap-1.5 px-4">
-                <img src={props.imageUrl} alt="user" className="rounded-full size-8 aspect-square" />
-                  <span>{props.name}</span>
+                <img
+                  src={props.imageUrl}
+                  alt="user"
+                  className="rounded-full size-8 aspect-square"
+                />
+                <span>{props.name}</span>
               </div>
+            )}
+          />
+
+          <ColumnDirective
+            field="email"
+            headerText="Email Address"
+            width="150"
+            textAlign="Left"
+          />
+
+          <ColumnDirective
+            field="dateJoined"
+            headerText="Date Joined"
+            width="120"
+            textAlign="Left"
+          />
+
+          <ColumnDirective
+            field="itineraryCreated"
+            headerText="Trip Created"
+            width="130"
+            textAlign="Left"
+          />
+
+          <ColumnDirective
+            field="status"
+            headerText="Type"
+            width="100"
+            textAlign="Left"
+            template={(props: UserData) => (
+              <article>
+                <div>
+                  <h3>{props.status}</h3>
+                </div>
+              </article>
             )}
           />
         </ColumnsDirective>
