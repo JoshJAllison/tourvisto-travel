0781eab6c2d9b0b47b56bd10b8d51b02f6d9093b
JoshJAllison <Joshua.Allison25@outlook.com>
2025-09-11 15:46:08 +0100

    Refactor CreateTrip component to enhance country selection with ComboBox, utilizing fetched country data and improving form structure.

--- app/routes/admin/create-trip.tsx
+++ app/routes/admin/create-trip.tsx
@@ -1,37 +1,51 @@
 import { ComboBox, ComboBoxComponent } from "@syncfusion/ej2-react-dropdowns";
 import { Header } from "../../../components";
-import type { Route } from './+types/create-trip'
+import type { Route } from "./+types/create-trip";
 
 export const loader = async () => {
-  const response = await fetch('https://restcountries.com/v3.1/all');
+  const response = await fetch("https://restcountries.com/v3.1/all");
   const data = await response.json();
-  return data;
+  return data.map((country: any) => ({
+    name: country.flag + country.name.common,
+    coordinates: country.latlng,
+    value: country.name.common,
+    openStreetMap: country.maps?.openStreetMap,
+  }))
 }
 
 const CreateTrip = ({ loaderData }: Route.ComponentProps) => {
   const handleSubmit = async () => {};
   const countries = loaderData as Country[];
-  console.log(countries);
+
+  const countryData = countries.map((country) => ({
+    text: country.name,
+    value: country.value,
+
+  }))
 
   return (
     <main className="flex flex-col gap-10 pb-20 wrapper">
-      <Header title="Add a New Trip" description="View and edit AI generated travel plans" />
+      <Header
+        title="Add a New Trip"
+        description="View and edit AI generated travel plans"
+      />
 
       <section className="mt-2.5 wrapper-md">
         <form className="trip-form" onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="country">
-              Country
-            </label>
-              <ComboBoxComponent 
-                  id="country"
-                  dataSource={['title', 'title1']}
-                />
+            <label htmlFor="country">Country</label>
+            <ComboBoxComponent 
+            id="country" 
+            dataSource={countryData} 
+            fields={{ text: 'text', value: 'value' }}
+            placeholder="Select a Country"
+            className="combo-box"
+            />
           </div>
         </form>
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default CreateTrip
+export default CreateTrip;
