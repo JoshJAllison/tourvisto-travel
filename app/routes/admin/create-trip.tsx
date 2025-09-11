b15cdb3cf0d924fd3d71ee098f050c4d5066d745
JoshJAllison <Joshua.Allison25@outlook.com>
2025-09-11 14:59:05 +0100

    Add form structure and submit handler in CreateTrip component for trip creation functionality.

--- app/routes/admin/create-trip.tsx
+++ app/routes/admin/create-trip.tsx
@@ -1,9 +1,17 @@
 import { Header } from "../../../components";
 
 const CreateTrip = () => {
+  const handleSubmit = async () => {};
+
   return (
     <main className="flex flex-col gap-10 pb-20 wrapper">
       <Header title="Add a New Trip" description="View and edit AI generated travel plans" />
+
+      <section className="mt-2.5 wrapper-md">
+        <form className="trip-form" onSubmit={handleSubmit}>
+
+        </form>
+      </section>
     </main>
   )
 }
