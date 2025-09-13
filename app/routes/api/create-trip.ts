bfaa9d0dd63b372e478cbfcac64efaf82def63c3
JoshJAllison <Joshua.Allison25@outlook.com>
2025-09-13 23:23:58 +0100

    Add request body parsing in CreateTrip action to extract trip details for processing.

--- app/routes/api/create-trip.ts
+++ app/routes/api/create-trip.ts
@@ -1,5 +1,13 @@
 import type { ActionFunctionArgs } from "react-router";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-
+    const {
+        country,
+        numberOfDays,
+        travelStyle,
+        interests,
+        budget,
+        groupType,
+        usedId,
+    } = await request.json();
 }
